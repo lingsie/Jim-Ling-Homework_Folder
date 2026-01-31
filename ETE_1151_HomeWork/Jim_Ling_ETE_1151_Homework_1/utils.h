46937240206859bdf40c1c8f490d2b8b9bf9c998
Lingsie <jimling0916@gmail.com>
2026-01-31 04:45:46 -0800

    Add files via upload

---
+++ ETE_1151_HomeWork/Jim_Ling_ETE_1151_Homework_1/utils.h
@@ -0,0 +1,16 @@
+// utils.cpp
+// Like utils.py :)
+
+
+#ifndef UTILS_H
+#define UTILS_H   // use utils.h to declare the helpers
+
+#include <string> // use the string library
+
+// helpers
+std::string charIntoBinary(char c); // helper for convert chart in to binary
+std::string intIntoBinart(int num); // change int
+std::string floatIntoBinart(float num); // 0.1 + 0.2 != 0.3 :)
+
+#endif
+// [EMD] //
